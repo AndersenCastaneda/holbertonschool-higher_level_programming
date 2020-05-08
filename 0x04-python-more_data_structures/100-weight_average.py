589115fa1534804ce968c064a909dfabfa388d8a
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-08 01:05:05 +0000

    Add task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    avg = 0
+    div = 0
+    for i, j in my_list:
+        print("i:{} j:{}".format(i, j))
+        avg += i * j
+        div += j
+    return avg / div
