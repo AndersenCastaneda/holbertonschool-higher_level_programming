192c424c23e8cb2d8022d601fbd1475d1938a33a
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-08 03:05:12 +0000

    Add task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or len(roman_string) == 0:
+        return 0
+    rmn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    for i in range(len(roman_string)):
+        if i > 0 and rmn[roman_string[i]] > rmn[roman_string[i - 1]]:
+            num -= rmn[roman_string[i]] - (2 * rmn[roman_string[i]])
+        else:
+            num += rmn[roman_string[i]]
+    return (num)
