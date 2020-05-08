a276b759e8471a0e82b8286e3a6665ce7b5ad2af
AndersenCastaneda <andersenpiano@gmail.com>
2020-05-08 00:44:27 +0000

    Hotfix key return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return "None"
-    return max(a_dictionary)
+        return None
+    m = None
+    for i in a_dictionary:
+        if not m or a_dictionary[i] > a_dictionary[m]:
+            m = i
+    return m
