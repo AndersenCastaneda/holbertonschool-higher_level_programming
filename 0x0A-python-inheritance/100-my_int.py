6054b95b60bbae5b4b61fcb3f0763b61857a6c77
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-02 23:09:39 +0000

    Add mandatory task

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Myint Module
+"""
+
+
+class MyInt(int):
+    """MyInt class"""
+
+    def __eq__(self, other):
+        """Reverse equality"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Reverse difference"""
+        return super().__eq__(other)
