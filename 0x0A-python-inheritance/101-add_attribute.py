64ea04bb87e8fce655ed60677d1e6a4b3e09ee60
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-03 00:50:11 +0000

    Add documentation

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,12 @@
 
 
 def add_attribute(obj, name, value):
-    """"""
+    """Adds attributes to a class
+    Parameters:
+        obj: Object(class)
+        name: attribute name
+        value: value for the attribute
+    """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, name, value)
