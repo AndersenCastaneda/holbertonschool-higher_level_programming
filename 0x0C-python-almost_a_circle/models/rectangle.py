ab20a08108aa968366ad0d55b3570b63cb9bae0a
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-07 02:23:00 +0000

    Add mandatory tasks

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+""" Rectangle Module """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle Class
+    Private instances attribute: __width, __height, __x, __y
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor
+        Parameters: self, width, heigth, x, y, id
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        rep = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
+        return rep
+
+    @property
+    def width(self):
+        """Returns width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width
+        Raises:
+            TypeError: value must be an integer
+            ValueError: value must be > 0
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height
+        Raises:
+            TypeError: value must be an integer
+            ValueError: value must be > 0
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Returns x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x
+        Raises:
+            TypeValue: value must be an integer
+            ValueError: value must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Returns y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y
+        Raises:
+            TypeValue: value must be an integer
+            ValueError: value must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Returns area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        print("".join("\n" for i in range(self.__y)), end="")
+        for i in range(self.__height):
+            print("".join(" " for i in range(self.__x)), end="")
+            print("".join("#" for i in range(self.__width)), end="")
+            print()
+
+    def update(self, *args, **kwargs):
+        """Update Class info
+        Parameters:
+            *args: (id, width, height, x, y) respetivaly.
+            **kwargs: {"id": , "width": , "height": , "x": , "y": }
+        """
+        if len(args) != 0:
+            if len(args) >= 1:
+                super().__init__(args[0])
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(value)
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
