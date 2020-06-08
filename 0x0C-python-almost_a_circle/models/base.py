a378c986acb85868c82cdc3726a9e772b4509b20
AndersenCastaneda <andersenpiano@gmail.com>
2020-06-08 19:10:19 +0000

    Add mandatory tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base Module """
 import json
+import os.path as path
 
 
 class Base:
@@ -28,8 +29,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of
-        list_dictionaries
+        """Returns the JSON string representation of list_dictionaries
+        Parameters: list_dictionaries
+        Raises:
+            TypeError: list_dictionaries must be a list of dictionaries
         """
         if list_dictionaries in [[], None]:
             return "[]"
@@ -37,8 +40,24 @@ class Base:
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation
+        Parameters: json_string
+        """
+        if json_string is None or json_string == "":
+            return []
+        else:
+            return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs
+        to a file
+        Parameters:
+            cls: class instance
+            list_object: list of object of type(cls)
+        """
         if list_objs is None or list_objs == []:
             data = "[]"
         else:
@@ -48,10 +67,26 @@ class Base:
         with open(filename, 'w') as file:
             file.write(data)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """Returns the list of the JSON string representation"""
-        if json_string is None or json_string == "":
-            return []
-        else:
-            return json.loads(json_string)
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set
+        Parameters:
+            cls: class instance
+            **dictionary: class info
+        """
+        obj = cls(1, 1)
+        obj.update(**dictionary)
+        return obj
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        objs = []
+        dict_list = []
+        if path.exists(filename):
+            with open(filename, 'r') as file:
+                dict_list = cls.from_json_string(file.read())
+                for dictionary in dict_list:
+                    objs.append(cls.create(**dictionary))
+                file.close()
+        return objs
