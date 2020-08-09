bdbf4bc7ca9458b019c4bf279217132fc1c56889
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-09 03:53:24 +0000

    Add task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy import update
+    from sqlalchemy.orm import sessionmaker
+    from model_state import State, Base
+
+    argv = sys.argv
+    argc = len(argv)
+
+    if argc == 4:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        up = session.query(State).filter(State.id == 2).update(
+            {State.name: "New Mexico"}, synchronize_session=False)
+        session.commit()
+        session.close()
