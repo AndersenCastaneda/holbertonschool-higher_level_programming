b506c37130040d257c9afa3faf93d97b7ff559bd
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-19 05:36:40 +0000

    Add task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,24 +2,34 @@
 """Peak Module"""
 
 
-def find_peak(list_of_integers):
+def find_peak(loi):
     """Finds a peak in list_of_integers"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if loi is None or loi == []:
         return None
 
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+    left = 0
+    right = len(loi) - 1
+
+    # Limit overflow
+    mid = left + ((right - left) // 2)
+
+    # If is only one element
+    if right == 1:
+        return loi[0]
+
+    # If there is only two elements return the bigger one
+    if right == 2:
+        return loi[0] if loi[0] > loi[1] else loi[1]
+
+    # If all elements has same value
+    if loi[mid] >= loi[mid - 1] and loi[mid] >= loi[mid + 1]:
+        return loi[mid]
+
+    # Find a peak on the rigth half
+    if mid > 0 and loi[mid] < loi[mid + 1]:
+        return find_peak(loi[mid:])
+
+    # Find a peak on the left half
+    if mid > 0 and loi[mid] < loi[mid - 1]:
+        return find_peak(loi[:mid])
