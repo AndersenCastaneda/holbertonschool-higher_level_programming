fdb33324334dcae006b696274518e71ce65a00e7
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-19 06:12:27 +0000

    Fix check 5

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,25 +11,21 @@ def find_peak(loi):
     left = 0
     right = len(loi) - 1
 
-    # Limit overflow
-    mid = left + ((right - left) // 2)
+    # Return the maximum value of two elements
+    if right == 0 or right == 1:
+        return max(loi)
 
-    # If is only one element
-    if right == 1:
-        return loi[0]
-
-    # If there is only two elements return the bigger one
-    if right == 2:
-        return loi[0] if loi[0] > loi[1] else loi[1]
+    # Cast to int
+    mid = right // 2
 
     # If all elements has same value
     if loi[mid] >= loi[mid - 1] and loi[mid] >= loi[mid + 1]:
         return loi[mid]
 
     # Find a peak on the rigth half
-    if mid > 0 and loi[mid] < loi[mid + 1]:
+    if loi[mid] < loi[mid + 1]:
         return find_peak(loi[mid:])
 
     # Find a peak on the left half
-    if mid > 0 and loi[mid] < loi[mid - 1]:
+    if loi[mid] < loi[mid - 1]:
         return find_peak(loi[:mid])
