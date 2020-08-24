f7926db4cb85ca18273c0f74cf40eab3453d8367
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-24 18:17:07 +0000

    Fix output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     request = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(request.text)))
-    print("\t - content: {}".format(request.text))
+    print("\t- content: {}".format(request.text))
