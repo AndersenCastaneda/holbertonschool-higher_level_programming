1b20ce1ad4111cbe15c0a8ee77c0fd73ed4debc1
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-24 22:05:08 +0000

    Fix edge case

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,18 +5,13 @@ and uses the Github API to display your id"""
 
 if __name__ == "__main__":
     import requests
-    from requests.auth import HTTPBasicAuth
     from sys import argv
 
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
     request = requests.get(url)
-    count = 0
-    for elem in request.json():
-        if type(elem) == dict:
-            count += 1
-            commits = elem.get('commit')
-            commit = commits.get('tree').get('sha')
-            author = commits.get('author').get('name')
-            print('{}: {}'.format(commit, author))
-            if count == 10:
-                break
+
+    commits = request.json()
+    for commit in commits[:10]:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print('{}: {}'.format(sha, author))
