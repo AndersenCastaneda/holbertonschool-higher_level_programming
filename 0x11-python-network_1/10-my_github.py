521c12e2253fe6588589af03cc9b348c70176240
AndersenCastaneda <andersenpiano@gmail.com>
2020-08-24 19:13:43 +0000

    Add task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,5 +5,9 @@ and uses the Github API to display your id"""
 
 if __name__ == "__main__":
     import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
 
-    pass
+    request = requests.get("https://api.github.com/users/{}".format(argv[1]),
+                           auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(request.json().get('id'))
