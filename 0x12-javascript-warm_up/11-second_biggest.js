8dad5fc83d03b53e61ead889fb7d90a1261c75ed
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-16 01:37:53 +0000

    Fix case 10

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 'use strict';
 let secBig = 0;
-let argv = process.argv.slice(2);
+const argv = process.argv.slice(2);
 if (argv.length > 1) {
   argv.sort();
   secBig = argv[argv.length - 2];
