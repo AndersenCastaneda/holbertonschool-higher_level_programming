4d633a794b51351ff337d3ca0bb3d61c497ac62f
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-21 18:12:39 -0500

    Fix output case 10

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const res = list.map((value, index) => value * index);
+const res = [];
+list.map((value, index) => res.push(value * index));
 console.log(list);
 console.log(res);
