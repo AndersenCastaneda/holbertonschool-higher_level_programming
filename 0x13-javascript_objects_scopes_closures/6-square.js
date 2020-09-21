8a8279bbf93781fe09defb39592245a52f89c65f
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-21 17:34:00 -0500

    Fix case 10

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const Sqr = require('./5-square');
 
 class Square extends Sqr {
   charPrint (c) {
-    const msg = (c === undefined) ? 'X' : 'C';
+    const msg = (c === undefined) ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
       console.log(msg.repeat(this.width));
     }
