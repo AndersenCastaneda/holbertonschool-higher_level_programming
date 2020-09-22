79adcdd9fa6faf4e7e19236cd088b15c15cfdbb7
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 14:10:06 -0500

    Add task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
-if (argv.length === 1) {
-  const fs = require('fs');
-  fs.readFile(argv[0], function read (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      process.stdout.write(data);
-    }
-  });
-}
+const fs = require('fs');
+fs.readFile(argv[0], function read (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
