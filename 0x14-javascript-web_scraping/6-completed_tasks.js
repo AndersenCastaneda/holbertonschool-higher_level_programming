b4653073fb87a1e03e55956c6b7d2bf5af035e71
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 16:42:28 -0500

    Check output case 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,20 +5,19 @@ const request = require('request');
 const url = process.argv[2];
 const userTasksCompleted = {};
 
-if (!url)
-	return;
-
-request(url, function taskCompleted (err, response, body) {
-  if (!err) {
-    const json = JSON.parse(body);
-    for (const task of json) {
-      if (!(task.userId in userTasksCompleted)) {
-        userTasksCompleted[task.userId] = 0;
+if (url) {
+  request(url, function taskCompleted (err, response, body) {
+    if (!err) {
+      const json = JSON.parse(body);
+      for (const task of json) {
+        if (!(task.userId in userTasksCompleted)) {
+          userTasksCompleted[task.userId] = 0;
+        }
+        if (task.completed === true) {
+          ++userTasksCompleted[task.userId];
+        }
       }
-      if (task.completed === true) {
-        ++userTasksCompleted[task.userId];
-      }
+      console.log(userTasksCompleted);
     }
-    console.log(userTasksCompleted);
-  }
-});
+  });
+}
