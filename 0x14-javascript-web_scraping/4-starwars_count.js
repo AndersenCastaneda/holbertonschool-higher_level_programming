bd23d090153116e388506936f434fe83e3ba67b8
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 16:03:42 -0500

    Improve search

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,20 +4,13 @@
 const request = require('request');
 const url = process.argv[2];
 let numberOfMovies = 0;
-const id = '18';
+const id = 'https://swapi-api.hbtn.io/api/people/18';
 
 if (url) {
-  request(url, function getTitle (err, response, body) {
+  request(id, function getTitle (err, response, body) {
     if (!err) {
-      const movies = JSON.parse(body).results;
-      for (const movie of movies) {
-        for (const character of movie.characters) {
-          if (character.includes(id)) {
-            numberOfMovies++;
-          }
-        }
-      }
-      console.log(numberOfMovies);
+      const films = JSON.parse(body).films;
+      console.log(films.length);
     }
   });
 }
