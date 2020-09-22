dec907789f3a5385b2939621d4fb956ab16c10a2
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 16:38:39 -0500

    Add task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Computes the number of tasks completed by user id.
+
+const request = require('request');
+const url = process.argv[2];
+const userTasksCompleted = {};
+
+request(url, function taskCompleted (err, response, body) {
+  if (!err) {
+    const json = JSON.parse(body);
+    for (const task of json) {
+      if (!(task.userId in userTasksCompleted)) {
+        userTasksCompleted[task.userId] = 0;
+      }
+      if (task.completed === true) {
+        ++userTasksCompleted[task.userId];
+      }
+    }
+    console.log(userTasksCompleted);
+  }
+});
