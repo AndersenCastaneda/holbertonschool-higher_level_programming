0fe1582ccddc09066c0e46f507ae548e28f87e74
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 23:06:38 -0500

    Check output cases

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,18 +6,22 @@ const movieID = process.argv[2];
 
 if (movieID) {
   const url = `https://swapi-api.hbtn.io/api/films/${movieID}`;
-  request(url, function taskCompleted (err, response, body) {
+  request(url, function taskCompleted(err, response, body) {
     if (!err) {
       const characters = JSON.parse(body).characters;
-      const length = characters.length;
-      for (let i = 0; i < length; i++) {
-        request(characters[i], function (err, response, body) {
-          if (!err) {
-            const info = JSON.parse(body);
-            console.log(info.name);
+      const list = [];
+      for (const character of characters) {
+        list.push(new Promise((resolve, reject) =>
+          request(character, function (err, response, body) {
+            if (err) {
+              reject(err);
+            }
+            const names = JSON.parse(body).name;
+            resolve(names);
           }
-        });
+        )));
       }
+      Promise.all(list).then((names) => names.forEach((name) => console.log(name)));
     }
   });
 }
