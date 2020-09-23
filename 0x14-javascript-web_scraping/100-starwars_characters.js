5ba91cb1f0f1d29fa31b8bca2dfa588a51adcf19
AndersenCastaneda <andersenpiano@gmail.com>
2020-09-22 22:34:35 -0500

    Add task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Prints all characters of a Star Wars movie
+
+const request = require('request');
+const movieID = process.argv[2];
+
+if (movieID) {
+  const url = `https://swapi-api.hbtn.io/api/films/${movieID}`;
+  request(url, function taskCompleted (err, response, body) {
+    if (!err) {
+      const characters = JSON.parse(body).characters;
+      for (const character of characters) {
+        request(character, function (err, response, body) {
+          if (!err) {
+            const info = JSON.parse(body);
+            console.log(info.name);
+          }
+        });
+      }
+    }
+  });
+}
